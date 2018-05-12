e42ac742b55499e31f2c25f50bdc9152d0daf3a4
liudongjie269 <liudongjie269@hotmail.com>
2018-05-12 23:08:33 +0800

    version 1

---
+++ Models/M_main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'M_main.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(317, 218)
+        self.verticalLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 291, 191))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.textEdit = QtWidgets.QTextEdit(self.verticalLayoutWidget)
+        self.textEdit.setStyleSheet("font: 11pt \"隶书\";")
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout.addWidget(self.textEdit)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+
---
+++ main.py
@@ -0,0 +1,14 @@
+from  Models.M_main import *
+from PyQt5 import QtCore,QtGui,QtWidgets
+import sys
+class my_main_ui(Ui_Dialog,QtWidgets.QDialog):
+    def __init__(self):
+        super(my_main_ui, self).__init__()
+        self.setupUi(self)
+        self.textEdit.textChanged.connect(lambda:print(self.textEdit.toPlainText()))
+
+if __name__ == '__main__':
+    app=QtWidgets.QApplication(sys.argv)
+    Win1=my_main_ui()
+    Win1.show()
+    app.exec_()
